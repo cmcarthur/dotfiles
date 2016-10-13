c4bfbe0d911e5eb6a059b49a610987c693f59bcc
Connor McArthur <cmcarthur@rjmetrics.com>
2016-10-13 17:32:15 -0400

    match color scheme to afterglow

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -8,16 +8,16 @@ import System.IO
 
 myDzenBar = "dzen2 -w '1920' -ta 'l'" ++ myStatusBarStyle
 myConkyBar = "conky -c ~/.xmonad/conkyrc | dzen2 -x '1920' -w '1920' -ta 'r'" ++ myStatusBarStyle
-myStatusBarStyle = " -h '24' -fg '#fdf6e3' -bg '#002b36'"
+myStatusBarStyle = " -h '24' -fg '#D0D0D0' -bg '#151515' -fn 'Source Code Pro'"
 
 main = do
     dzenLeftBar <- spawnPipe myDzenBar
     dzenRightBar <- spawnPipe myConkyBar
     xmonad $ gnomeConfig {
 		modMask = mod4Mask -- mod key = win
-		, workspaces          = [ "1:emacs", "2:web", "3:mail", "4:trello", "5:shell", "6:dev", "7:tunes" ]
+		, workspaces          = [ "1:local", "2:vm", "3:prod", "4:browser", "5:personal" ]
 		, logHook             = myLogHook dzenLeftBar >> fadeInactiveLogHook 0xdddddddd
-      
+
 	}
 	`additionalKeysP`
 	 [
@@ -29,12 +29,12 @@ main = do
 myLogHook :: Handle -> X ()
 myLogHook h = dynamicLogWithPP $ defaultPP
     {
-        ppCurrent           =   dzenColor "#2aa198" "#002b36" . pad
-      , ppVisible           =   dzenColor "#859900" "#002b36" . pad
-      , ppHidden            =   dzenColor "#fdf6e3" "#002b36" . pad
-      , ppUrgent            =   dzenColor "#fdf6e3" "#002b36" . pad
-      , ppWsSep             =   " "
-      , ppSep               =   "  |  "
-      , ppTitle             =   (" " ++) . dzenColor "#fdf6e3" "#002b36" . dzenEscape
+        ppCurrent           =   dzenColor "#7E8D50" "#151515" . pad
+      , ppVisible           =   dzenColor "#E5B566" "#151515" . pad
+      , ppHidden            =   dzenColor "#6C99BA" "#151515" . pad
+      , ppUrgent            =   dzenColor "#6C99BA" "#151515" . pad
+      , ppWsSep             =   ""
+      , ppSep               =   " | "
+      , ppTitle             =   ("" ++) . dzenColor "#D0D0D0" "#151515" . dzenEscape
       , ppOutput            =   hPutStrLn h
     }
